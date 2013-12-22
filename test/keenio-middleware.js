bff33de7785b415f39cfe77cc2bc93c65269387e
Seb Insua <me@sebinsua.com>
2013-12-22 20:36:19 +0000

    Tests commented out - begginning coding tomorrow.

--- test/keenio-middleware.js
+++ test/keenio-middleware.js
@@ -21,8 +21,6 @@ var mockKeenClientModule = {
 
 var keenioMiddleware = proxyquire('../lib/keenio-middleware', { "keen.io": mockKeenClientModule });
 
-// @todo: Write some tests with sensible names. Upgrade the broken describe, and change it. Then read through all of them.
-
 // @todo: Update the README.md.
 // @todo: Before I release, take a look at some node.js projects I know about and see how it would be used with them.
 //        Also take a look at something like this for ideas on interface: https://segment.io/libraries/node
@@ -35,6 +33,7 @@ describe("keenioMiddleware", function () {
   describe("configure()", function () {
     // @todo: Look into some way of validating arguments in JavaScript. Nice way of doing so.
 
+    /*
     it("should error if no configuration is passed in", function () {
       true.should.be.false;
     });
@@ -50,17 +49,40 @@ describe("keenioMiddleware", function () {
       true.should.be.false;
     });
 
+    it("should return a valid middleware if handleAll() is executed after valid configuration", function () {
+      // @todo: How to configure? Can I have modes and request-responses?
+      // @todo: Way of tagging event collection + and replacing event collection name with better name.
+      true.should.be.false;
+    });
+
     it("should error if handle() is executed before calling configure()", function () {
       true.should.be.false;
     });
+
+    it("should error if handleAll() is executed before calling configure()", function () {
+      true.should.be.false;
+    });
+    */
+
   });
 
   describe("identify()", function () {
     // @todo: Describe identify. Can I detect which user is logged in (and store this in a key-value)?
     //        Use the word 'track' in some places.
+
+    /*
+    it("should be able to get out data from req.user if this has been set", function () {
+
+    });
+
+    it("should be able to fallback to getting data from a session/header variable if user was not set", function () {
+
+    });
+    */
+
   });
 
-  describe("handleAll() - all routes", function () {
+  describe("handleAll() - all preconfigured routes", function () {
     var app;
 
     beforeEach(function () {
@@ -83,8 +105,95 @@ describe("keenioMiddleware", function () {
     });
 
     // @todo: Sensible defaults for names, params, queries, key-values, etc.
-    // @todo: Way of tagging event collection + and replacing event collection name with better name.
 
+    it("should send valid event data to keen.io on making a json body request", function (done) {
+      keenioMiddleware.keenClient.makeRequest = function testRequestData (eventCollection, event) {
+        eventCollection.should.equal("api--test");
+        event.should.have.property('intention');
+        event.intention.should.eql({
+          path: '/test',
+          query: {},
+          body: {
+            user: "seb"
+          }
+        });
+      };
+
+      request(app).post('/test')
+                  .send({ "user": "seb" })
+                  .expect('{\n  "user": "seb"\n}', done);
+    });
+    
+    /*  
+    it("should send valid event data to keen.io on making a params request", function (done) {
+      true.should.be.false;
+    });
+
+    it("should send valid event data to keen.io on making a query request", function (done) {
+      true.should.be.false;
+    });
+
+    it("should track a user if they could be identified from a request", function (done) {
+      true.should.be.false;
+    });
+
+    it("should send no reaction to keen.io if application/json is not specified as the response", function (done) {
+      // @todo: Handle json response. Handle non-json response.
+      true.should.be.false;
+    });
+
+    it("should send a reaction to keen.io if application/json is specified as the response", function (done) {
+      // @todo: Handle json response. Handle non-json response.
+      true.should.be.false;
+    });
+
+    it("should not send header data to keen.io by default", function (done) {
+      true.should.be.false;
+    });
+
+    it("should send header data to keen.io if the configuration mandated this", function (done) {
+      true.should.be.false;
+    });
+
+    it("should ignore events that are explicity denied in the configuration", function (done) {
+      true.should.be.false;
+    });
+
+    it("should allow you to set the eventCollectionName for a route from the configuration", function (done) {
+      true.should.be.false;
+    });
+
+    it("should allow you to tag the event for a route from the configuration", function (done) {
+      true.should.be.false;
+    });
+    */
+
+  });
+
+  describe("handle() - specific route", function () {
+    var app;
+
+    beforeEach(function () {
+      keenioMiddleware.configure({
+        projectId: "<fake-project-id>",
+        writeKey: "<fake-write-key>"
+      });
+
+      app = express();
+      app.configure(function () {
+        app.use(express.bodyParser());
+        app.use(app.router);
+      });
+
+      app.post('/test', function (req, res) {
+        var requestBody = req.body;
+        res.send(requestBody);
+      });
+    });
+
+    // @todo: Decorator-style version should be able to be used instead of the general middleware version.
+
+    /*
     it("should send valid event data to keen.io on making a json body request", function (done) {
       keenioMiddleware.keenClient.makeRequest = function testRequestData (eventCollection, event) {
         eventCollection.should.equal("api--test");
@@ -111,6 +220,10 @@ describe("keenioMiddleware", function () {
       true.should.be.false;
     });
 
+    it("should track a user if they could be identified from a request", function (done) {
+      true.should.be.false;
+    });
+
     it("should send no reaction to keen.io if application/json is not specified as the response", function (done) {
       // @todo: Handle json response. Handle non-json response.
       true.should.be.false;
@@ -121,31 +234,14 @@ describe("keenioMiddleware", function () {
       true.should.be.false;
     });
 
-  });
-
-  describe("handle() - specific route", function () {
-    var app;
-
-    beforeEach(function () {
-      keenioMiddleware.configure({
-        projectId: "<fake-project-id>",
-        writeKey: "<fake-write-key>"
-      });
-
-      app = express();
-      app.configure(function () {
-        app.use(express.bodyParser());
-        app.use(app.router);
-      });
-
-      app.post('/test', function (req, res) {
-        var requestBody = req.body;
-        res.send(requestBody);
-      });
+    it("should allow you to set the eventCollectionName with the first argument", function (done) {
+      true.should.be.false;
     });
 
-    // @todo: Decorator-style version should be able to be used instead of the general middleware version.
-    // @todo: Way of tagging event collection + and replacing event collection name with better name.
+    it("should allow you to tag the event with the second argument", function (done) {
+      true.should.be.false;
+    });    
+    */
   });
 
 });
