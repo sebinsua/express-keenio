15935e4dd3b81e1dfe67d57675533d6cbc8f3570
Seb Insua <me@sebinsua.com>
2014-01-06 18:02:22 +0000

    Let's alter how params are picked up so that it works with the all handler. Let's also fix the tests so that they use a spy, and do not fail in the way demonstrated previously in this gist: https://gist.github.com/sebinsua/8118001

--- test/keenio-middleware.js
+++ test/keenio-middleware.js
@@ -8,24 +8,22 @@ var mockKeenClientModule = {
 
   configure: function (options) {
     return {
-      makeRequest: undefined,
-      addEvent: function (eventCollection, event, callback) {
-        // Overriding this method stops the unit tests from hitting keen.io.
-        // Note: a defined this.makeRequest allows us to execute some assertions from the unit tests.
-        if (this.makeRequest) {
-          this.makeRequest(eventCollection, event, callback);
-        }
-      }
+      addEvent: function (eventCollection, event, callback) {}
     }; 
   }
 
 };
 
+// @todo: Fix these event identity things - they're too all-encompassing.
+// @todo: How to configure? Can I have modes and request-responses? A new config format.
+// @todo: Handle non-json response.
+// @todo: Restructure the format of intention that we send to keen.io.
+
 // @todo: Update the README.md.
 // @todo: Before I release, take a look at some node.js projects I know about and see how it would be used with them.
 //        Are we really dealing with events? Yes.
 //        Should event be human-readable? Yes. Thus tagged.
-//        Were properties a intent-react generalisation? Yes.
+//        Were properties a intent-react generalisation? Yes - but the meaning might not be clear.
 
 describe("keenioMiddleware", function () {
   
@@ -76,7 +74,6 @@ describe("keenioMiddleware", function () {
     });
 
     it("should return a valid middleware if handle() is executed after valid configuration", function () {
-      // @todo: How to configure? Can I have modes and request-responses?
       var configuration = {
         client: {
           projectId: '<test>',
@@ -95,7 +92,6 @@ describe("keenioMiddleware", function () {
     });
     
     it("should return a valid middleware if handleAll() is executed after valid configuration", function () {
-      // @todo: How to configure? Can I have modes and request-responses?
       var configuration = {
         client: {
           projectId: '<test>',
@@ -176,68 +172,161 @@ describe("keenioMiddleware", function () {
         app.use(express.json());
         app.use(express.urlencoded()); // note: these two replace: app.use(express.bodyParser());
                                        // see:  http://stackoverflow.com/questions/19581146/how-to-get-rid-of-connect-3-0-deprecation-alert
-
-        app.use(keenioMiddleware.handleAll());
-        app.use(app.router);
+        app.use(keenioMiddleware.handleAll());        
+        app.use(app.router);   
       });
 
       app.post('/test', function (req, res) {
         var requestBody = req.body;
         res.send(requestBody);
       });
+      app.post('/params/:userId/:someParam/:someOtherParam', function (req, res) {
+        var requestBody = req.body;
+        res.send(requestBody);
+      });
     });
 
     it("should send valid event data to keen.io on making a json body request", function (done) {
-      keenioMiddleware.keenClient.makeRequest = function testRequestData (eventCollection, event) {
-        // @todo: AssertionErrors aren't causing anything to happen due to supertest.
-        eventCollection.should.equal("api--test");
-        event.should.have.property('intention');
-        event.intention.should.eql({
-          path: '/test',
-          query: {},
-          body: {
-            user: "seb"
-          }
-        });
-        event.reaction.should.eql({ user: "seb" });
-      };
+      var testRequest = sinon.spy();
+      keenioMiddleware.keenClient.addEvent = testRequest;
 
       request(app).post('/test')
                   .send({ "user": "seb" })
-                  .expect('{\n  "user": "seb"\n}', done);
+                  .expect('{\n  "user": "seb"\n}', function () {
+
+                    var callArgs, eventCollection, event;
+                    callArgs = testRequest.getCall(0).args;
+                    eventCollection = callArgs[0];
+                    event = callArgs[1];
+
+                    eventCollection.should.equal("api--test");
+                    event.should.have.property('intention');
+                    event.intention.should.eql({
+                      path: '/test',
+                      query: {},
+                      body: {
+                        user: "seb"
+                      },
+                      params: {}
+                    });
+                    event.reaction.should.eql({ user: "seb" });
+
+                    done();
+                  });
     });
     
-    // @todo: Sensible defaults for names, params, queries, key-values, etc.
-    // @todo: Way of tagging event collection + and replacing event collection name with better name.
-
-    /*  
     it("should send valid event data to keen.io on making a params request", function (done) {
-      true.should.be.false;
+      var testRequest = sinon.spy();
+      keenioMiddleware.keenClient.addEvent = testRequest;
+
+      request(app).post('/params/5/7/8')
+                  .send({ "user": "seb" })
+                  .expect('{\n  "user": "seb"\n}', function () {
+                    
+                    var callArgs, eventCollection, event;
+                    callArgs = testRequest.getCall(0).args;
+                    eventCollection = callArgs[0];
+                    event = callArgs[1];
+
+                    eventCollection.should.equal("api--params/5/7/8");
+                    event.should.have.property('intention');
+                    event.intention.should.eql({
+                      path: '/params/5/7/8',
+                      body: {
+                        user: "seb"
+                      },
+                      query: {},
+                      params: { userId: '5', someParam: '7', someOtherParam: '8' }
+                    });
+                    event.should.have.property("reaction");
+                    event.reaction.should.eql({
+                      user: "seb"
+                    });
+
+                    done();
+                  });
     });
 
     it("should send valid event data to keen.io on making a query request", function (done) {
-      true.should.be.false;
+      var testRequest = sinon.spy();
+      keenioMiddleware.keenClient.addEvent = testRequest;
+
+      request(app).post('/test?someArgument=2&anotherArgument=4')
+                  .send({ "user": "seb" })
+                  .expect('{\n  "user": "seb"\n}', function () {
+                    
+                    var callArgs, eventCollection, event;
+                    callArgs = testRequest.getCall(0).args;
+                    eventCollection = callArgs[0];
+                    event = callArgs[1];
+
+                    eventCollection.should.equal("api--test");
+                    event.should.have.property('intention');
+                    event.intention.should.eql({
+                      path: '/test',
+                      query: {
+                        someArgument: '2',
+                        anotherArgument: '4'
+                      },
+                      body: {
+                        user: "seb"
+                      },
+                      params: {}
+                    });
+                    event.should.have.property("reaction");
+                    event.reaction.should.eql({
+                      user: "seb"
+                    });
+
+                    done();
+                  });
     });
 
     it("should track a user if they could be identified from a request", function (done) {
-      true.should.be.false;
+      var testRequest = sinon.spy();
+      keenioMiddleware.keenClient.addEvent = testRequest;
+
+      request(app).post('/test')
+                  .send({ "user": "seb" })
+                  .expect('{\n  "user": "seb"\n}', function () {
+                    var event = testRequest.getCall(0).args[1];
+
+                    // event.identity.should.eql();
+
+                    done();
+                  });
     });
 
     it("should send no reaction to keen.io if application/json is not specified as the response", function (done) {
-      // @todo: Handle json response. Handle non-json response.
-      true.should.be.false;
+      var makeRequest = sinon.spy();
+      keenioMiddleware.keenClient.makeRequest = makeRequest;
+
+      request(app).post('/test')
+                  .send('{ "user": "seb" }')
+                  .expect('{\n  "user": "seb"\n}', function () {
+                    makeRequest.calledOnce.should.equal(false);
+                    done();
+                  });
     });
 
     it("should send a reaction to keen.io if application/json is specified as the response", function (done) {
-      // @todo: Handle json response. Handle non-json response.
+      var makeRequest = sinon.spy();
+      keenioMiddleware.keenClient.addEvent = makeRequest;
+
+      request(app).post('/test')
+                  .send({ "user": "seb" })
+                  .expect('{\n  "user": "seb"\n}', function () {
+                    makeRequest.calledOnce.should.equal(true);
+                    done();
+                  });
+    });
+
+    /*
+    it("should not send identity data to keen.io by default", function (done) {
       true.should.be.false;
     });
 
-    it("should not send header data to keen.io by default", function (done) {
-      true.should.be.false;
-    });
-
-    it("should send header data to keen.io if the configuration mandated this", function (done) {
+    it("should send specific identity data to keen.io if the configuration mandated this", function (done) {
       true.should.be.false;
     });
 
@@ -283,56 +372,207 @@ describe("keenioMiddleware", function () {
         res.send(requestBody);
       });      
 
-      // @todo: AssertionErrors aren't causing anything to happen when run inside supertest.
-      keenioMiddleware.keenClient.makeRequest = function testRequestData (eventCollection, event) {
-        eventCollection.should.equal("eventCollectionName");
-        event.tag.should.equal("Posted to test");
-        event.should.have.property('intention');
-        event.intention.should.eql({
-          path: '/test',
-          query: {},
-          body: {
-            user: "seb"
-          }
-        });
-      };
+      var testRequest = sinon.spy();
+      keenioMiddleware.keenClient.addEvent = testRequest;
 
       request(app).post('/test')
                   .send({ "user": "seb" })
-                  .expect('{\n  "user": "seb"\n}', done);
+                  .expect('{\n  "user": "seb"\n}', function () {
+                    
+                    var callArgs, eventCollection, event;
+                    callArgs = testRequest.getCall(0).args;
+                    eventCollection = callArgs[0];
+                    event = callArgs[1];                    
+
+                    eventCollection.should.equal("eventCollectionName");
+                    event.tag.should.equal("Posted to test");
+                    event.should.have.property('intention');
+                    event.intention.should.eql({
+                      path: '/test',
+                      query: {},
+                      body: {
+                        user: "seb"
+                      },
+                      params: {}
+                    });
+                    event.should.have.property("reaction");
+                    event.reaction.should.eql({
+                      user: "seb"
+                    });
+
+                    done();
+                  });
     });
     
-    /*
     it("should send valid event data to keen.io on making a params request", function (done) {
-      true.should.be.false;
+      app.post('/params/:userId/:someParam/:someOtherParam', keenioMiddleware.handle(), function (req, res) {
+        var requestBody = req.body;
+        res.send(requestBody);
+      });
+
+      var testRequest = sinon.spy();
+      keenioMiddleware.keenClient.addEvent = testRequest;
+
+      request(app).post('/params/5/7/8')
+                  .send({ "user": "seb" })
+                  .expect('{\n  "user": "seb"\n}', function () {
+                    
+                    var callArgs, eventCollection, event;
+                    callArgs = testRequest.getCall(0).args;
+                    eventCollection = callArgs[0];
+                    event = callArgs[1];                    
+
+                    eventCollection.should.equal("api--params/5/7/8");
+                    event.should.have.property('intention');
+                    event.intention.should.eql({
+                      path: '/params/5/7/8',
+                      query: {},
+                      body: {
+                        user: "seb"
+                      },
+                      params: { userId: '5', someParam: '7', someOtherParam: '8' }
+                    });
+                    event.should.have.property("reaction");
+                    event.reaction.should.eql({
+                      user: "seb"
+                    });                    
+
+                    done();
+                  });
     });
 
     it("should send valid event data to keen.io on making a query request", function (done) {
-      true.should.be.false;
-    });
+      app.get('/test', keenioMiddleware.handle(), function (req, res) {
+        var requestBody = req.body;
+        res.send(requestBody);
+      });
+      var testRequest = sinon.spy();
+      keenioMiddleware.keenClient.addEvent = testRequest;
 
+      request(app).get('/test?someArgument=2&anotherArgument=4')
+                  .send({ "user": "seb" })
+                  .expect('{\n  "user": "seb"\n}', function () {
+                    
+                    var callArgs, eventCollection, event;
+                    callArgs = testRequest.getCall(0).args;
+                    eventCollection = callArgs[0];
+                    event = callArgs[1];                    
+
+                    eventCollection.should.equal("api--test");
+                    event.should.have.property('intention');
+                    event.intention.should.eql({
+                      path: '/test',
+                      query: {
+                        someArgument: '2',
+                        anotherArgument: '4'
+                      },
+                      body: {
+                        user: "seb"
+                      },
+                      params: {}
+                    });
+                    event.should.have.property("reaction");
+                    event.reaction.should.eql({
+                      user: "seb"
+                    });
+
+                    done();
+                  });
+    });
+    
     it("should track a user if they could be identified from a request", function (done) {
-      true.should.be.false;
+      app.get('/test', keenioMiddleware.handle(), function (req, res) {
+        var requestBody = req.body;
+        res.send(requestBody);
+      });
+
+      var testRequest = sinon.spy();
+      keenioMiddleware.keenClient.addEvent = testRequest;
+
+      request(app).get('/test')
+                  .send({ "user": "seb" })
+                  .expect('{\n  "user": "seb"\n}', function () {
+                    var event = testRequest.getCall(0).args[1];
+
+                    // event.identity.should.eql();
+
+                    done();
+                  });
     });
 
     it("should send no reaction to keen.io if application/json is not specified as the response", function (done) {
-      // @todo: Handle json response. Handle non-json response.
-      true.should.be.false;
+      app.get('/test', keenioMiddleware.handle(), function (req, res) {
+        var requestBody = req.body;
+        res.send(requestBody);
+      });
+
+      var makeRequest = sinon.spy();
+      keenioMiddleware.keenClient.addEvent = makeRequest;
+
+      request(app).get('/test')
+                  .send('{ "user": "seb" }')
+                  .expect('{\n  "user": "seb"\n}', function () {
+                    makeRequest.calledOnce.should.equal(false);
+                    done();
+                  });
     });
 
     it("should send a reaction to keen.io if application/json is specified as the response", function (done) {
-      // @todo: Handle json response. Handle non-json response.
-      true.should.be.false;
+      app.get('/test', keenioMiddleware.handle(), function (req, res) {
+        var requestBody = req.body;
+        res.send(requestBody);
+      });
+
+      var makeRequest = sinon.spy();
+      keenioMiddleware.keenClient.addEvent = makeRequest;
+
+      request(app).get('/test')
+                  .send({ "user": "seb" })
+                  .expect('{\n  "user": "seb"\n}', function () {
+                    makeRequest.calledOnce.should.equal(true);
+                    done();
+                  });
     });
 
     it("should allow you to set the eventCollectionName with the first argument", function (done) {
-      true.should.be.false;
+      app.post('/test', keenioMiddleware.handle('eventCollectionName'), function (req, res) {
+        var requestBody = req.body;
+        res.send(requestBody);
+      });      
+
+      var testRequest = sinon.spy();
+      keenioMiddleware.keenClient.addEvent = testRequest;
+
+      request(app).post('/test')
+                  .send({ "user": "seb" })
+                  .expect('{\n  "user": "seb"\n}', function () {
+                    var eventCollection = testRequest.getCall(0).args[0];
+
+                    eventCollection.should.equal("eventCollectionName");
+
+                    done();
+                  });
     });
 
     it("should allow you to tag the event with the second argument", function (done) {
-      true.should.be.false;
-    });    
-    */
+      app.post('/test', keenioMiddleware.handle(null, "Event tag"), function (req, res) {
+        var requestBody = req.body;
+        res.send(requestBody);
+      });      
+
+      var testRequest = sinon.spy();
+      keenioMiddleware.keenClient.addEvent = testRequest;
+
+      request(app).post('/test')
+                  .send({ "user": "seb" })
+                  .expect('{\n  "user": "seb"\n}', function () {
+                    var event = testRequest.getCall(0).args[1];
+
+                    event.tag.should.equal("Event tag");
+
+                    done();
+                  });
+    });
   });
 
 });
