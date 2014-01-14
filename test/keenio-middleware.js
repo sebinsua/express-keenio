faa0e62b12f50f5102aed5c68eeca096ed6832ab
Seb Insua <me@sebinsua.com>
2014-01-14 01:52:42 +0000

    Code is very messy now but the tests are at least proving that it works... :)

--- test/keenio-middleware.js
+++ test/keenio-middleware.js
@@ -14,12 +14,13 @@ var mockKeenClientModule = {
 
 };
 
-// Are you solving your core problem? YES.
+// @todo: Find out why the is.json thing has to be used... Can we avoid?
+// @todo: Abstract: Handler, Identify, Request, Response parser.
+// @todo: Write some code to ensure that handleAll config gets overridden by individual middleware functions and not the other way around.
 
-// Have you got sensible coarse defaults?
-// Easy to reject routes?
+// --- 
 
-// @todo: Come up with a configuration format.
+// @todo: Update the README.md.
 /*
 {
   client: {
@@ -33,26 +34,17 @@ var mockKeenClientModule = {
     parseRequestBody: function (body) {},
     parseResponseBody: function (body) {}
   },
+  excludeRoutes: [
+    { method: 'GET', route: 'route-name' }
+  ],
   routes: [
     { method: 'GET', route: 'route-name-1', eventCollectionName: '', tag: '' },
     { method: 'POST', route: 'route-name-2', eventCollectionName: '', tag: '' }
   ],
-  excludeRoutes: [
-    'route-name'
-  ],
 }
 */
-// @todo: Some way of parsing the request and response body differently.
-
-// @todo: Create way of specifying event list from route names.
-// @todo: Create way of specifiying eventCollectionNames and tags for this list.
-// @todo: Find out why the is.json thing has to be used... Can we avoid?
-
-// --- 
-
-// @todo: Update the README.md.
 // @todo: Before I release, take a look at some node.js projects I know about and see how it would be used with them.
-// @todo: Abstract: Handler, Identify, Request, Response parser.
+
 
 describe("keenioMiddleware", function () {
   
@@ -63,7 +55,7 @@ describe("keenioMiddleware", function () {
 
   describe("configure()", function () {
 
-    it("should error if no/bad configuration is passed in", function () {
+    it("should error if invalid configuration was passed in", function () {
       // Test the client configuration.
       var tests = [
         {
@@ -99,6 +91,45 @@ describe("keenioMiddleware", function () {
           var configure = keenioMiddleware.configure.bind(keenioMiddleware);
           configure(test.configuration);
         }).should.throw(Error, test.errorMessage);
+      });
+    });
+
+    it("should error if both routes and excludeRoutes are passed in", function () {
+      var configuration = {
+        client: {
+          projectId: '<test>',
+          writeKey: '<test>'
+        },
+        excludeRoutes: [],
+        routes: []
+      };
+
+      (function () {
+        var configure = keenioMiddleware.configure.bind(keenioMiddleware);
+        configure(configuration);
+      }).should.throw(Error, "You must only specify routes or excludeRoutes, never both.");
+    });
+
+    it("should not error if only routes or excludeRoutes was passed in", function () {
+      var tests = [{
+        client: {
+          projectId: '<test>',
+          writeKey: '<test>'
+        },
+        routes: []
+      }, {
+        client: {
+          projectId: '<test>',
+          writeKey: '<test>'
+        },
+        excludeRoutes: []
+      }];
+
+      tests.forEach(function (configuration) {
+        (function () {
+          var configure = keenioMiddleware.configure.bind(keenioMiddleware);
+          configure(configuration);
+        }).should.not.throw(Error, "You must only specify routes or excludeRoutes, never both.");
       });
     });
 
@@ -184,6 +215,82 @@ describe("keenioMiddleware", function () {
     });
   });
 
+  describe("_isExcludedRoute()", function () {
+    var configuration = {
+      client: {
+        projectId: '<test>',
+        writeKey: '<test>'
+      },
+      excludeRoutes: [
+        { route: "/other-route", method: "post" },
+        { route: "/test", method: "get" },
+        { route: "/test", method: "post" }
+      ],
+    };
+
+    it("should return true if a route is in the excluded list", function () {
+      var route = {
+        path: "/test",
+        method: "get"
+      };
+
+      keenioMiddleware.configure(configuration);
+      keenioMiddleware._isExcludedRoute(route).should.be.true;
+    });
+
+    it("should return false if a route is not in the excluded list", function () {
+      var route = {
+        path: "/test",
+        method: "options"
+      };
+
+      keenioMiddleware.configure(configuration);
+      keenioMiddleware._isExcludedRoute(route).should.be.false;
+    });
+  });
+
+  describe("_getEventCollectionMetadataForRoute()", function () {
+    var configuration = {
+      client: {
+        projectId: '<test>',
+        writeKey: '<test>'
+      },
+      routes: [
+        { route: "/other-route", method: "post" },
+        { route: "/test", method: "get", eventCollectionName: "specialEventCollectionName" },
+        { route: "/test", method: "post", tag: "specialTag" }
+      ],
+    };
+
+    it("should just return undefined if there was nothing relevant in the configuration", function () {
+      var route = {
+        path: "/test",
+        method: "options"
+      };
+
+      keenioMiddleware.configure(configuration);
+      var metadata = keenioMiddleware._getEventCollectionMetadataForRoute(route);
+      metadata.should.be.false;
+    });
+
+    it("should return valid data if the route matched with one that had metadata", function () {
+      var metadata, route_a = {
+        path: "/test",
+        method: "get"
+      }, route_b = {
+        path: "/other-route",
+        method: "post"
+      };
+
+      keenioMiddleware.configure(configuration);
+      metadata = keenioMiddleware._getEventCollectionMetadataForRoute(route_a);
+      metadata.should.eql({ eventCollectionName: "specialEventCollectionName" });
+
+      metadata = keenioMiddleware._getEventCollectionMetadataForRoute(route_b);
+      metadata.should.eql({});
+    });
+  });
+
   describe("identify()", function () {
 
     it("should be able to get out data from req.user if this has been set", function () {
@@ -239,7 +346,7 @@ describe("keenioMiddleware", function () {
 
   });
 
-  describe("handleAll() - all preconfigured routes", function () {
+  describe("handleAll() - default routes", function () {
     var app;
 
     beforeEach(function () {
@@ -247,7 +354,10 @@ describe("keenioMiddleware", function () {
         client: {
           projectId: "<fake-project-id>",
           writeKey: "<fake-write-key>"
-        }
+        },
+        excludeRoutes: [
+          { route: '/disabled-insecure-route', method: 'get' }
+        ]
       });
 
       app = express();
@@ -264,6 +374,10 @@ describe("keenioMiddleware", function () {
         res.send(requestBody);
       });
       app.post('/params/:userId/:someParam/:someOtherParam', function (req, res) {
+        var requestBody = req.body;
+        res.send(requestBody);
+      });
+      app.get('/disabled-insecure-route', function (req, res) {
         var requestBody = req.body;
         res.send(requestBody);
       });
@@ -399,11 +513,10 @@ describe("keenioMiddleware", function () {
       request(app).post('/test')
                   .send({ "user": "seb" })
                   .expect('{\n  "user": "seb"\n}', function () {
-                    makeRequest.calledOnce.should.equal(true);
+                    makeRequest.calledOnce.should.be.true;
                     done();
                   });
     });
-
 
     it("should not send identity data to keen.io by default", function (done) {
       var testRequest = sinon.spy();
@@ -420,24 +533,121 @@ describe("keenioMiddleware", function () {
                     done();
                   });
     });
+    
+    it("should ignore events that are explicity denied in the configuration", function (done) {
+      var testRequest = sinon.spy();
+      keenioMiddleware.keenClient.addEvent = testRequest;
 
-    /*
-    it("should send specific identity data to keen.io if the configuration mandated this", function (done) {
-      true.should.be.false;
+      request(app).get('/disabled-insecure-route')
+                  .send({ "user": "seb" })
+                  .expect('{\n  "user": "seb"\n}', function () {
+                    testRequest.calledOnce.should.be.false;
+                    done();
+                  });
     });
 
-    it("should ignore events that are explicity denied in the configuration", function (done) {
-      true.should.be.false;
+  });
+
+  describe("handleAll() - preconfigured routes + altered defaults", function () {
+    var app;
+
+    beforeEach(function () {
+      keenioMiddleware.configure({
+        client: {
+          projectId: "<fake-project-id>",
+          writeKey: "<fake-write-key>"
+        },
+        defaults: {
+          generateIdentity: function (req) { return { test: "lies all lies - see later test" } }
+        },
+        routes: [
+          { route: '/params/:userId/:someParam/:someOtherParam', method: 'post', eventCollectionName: "testEventCollectionName" },
+          { route: '/test', method: 'get', tag: "testTagName" }  
+        ]
+      });
+
+      app = express();
+      app.configure(function () {
+        app.use(express.json());
+        app.use(express.urlencoded()); // note: these two replace: app.use(express.bodyParser());
+                                       // see:  http://stackoverflow.com/questions/19581146/how-to-get-rid-of-connect-3-0-deprecation-alert
+        app.use(keenioMiddleware.handleAll());        
+        app.use(app.router);   
+      });
+
+      app.get('/test', function (req, res) {
+        var requestBody = req.body;
+        res.send(requestBody);
+      });
+      app.post('/test', function (req, res) {
+        var requestBody = req.body;
+        res.send(requestBody);
+      });
+      app.post('/params/:userId/:someParam/:someOtherParam', function (req, res) {
+        var requestBody = req.body;
+        res.send(requestBody);
+      });
+    });
+
+    it("should no longer pick up events from routes not defined in the routes configuration", function (done) {
+      var testRequest = sinon.spy();
+      keenioMiddleware.keenClient.addEvent = testRequest;
+
+      request(app).post('/test')
+                  .send({ "user": "seb" })
+                  .expect('{\n  "user": "seb"\n}', function () {
+                    testRequest.calledOnce.should.be.false;
+                    done();
+                  });
     });
 
     it("should allow you to set the eventCollectionName for a route from the configuration", function (done) {
-      true.should.be.false;
+      var testRequest = sinon.spy();
+      keenioMiddleware.keenClient.addEvent = testRequest;
+
+      request(app).post('/params/5/6/7')
+                  .send({ "user": "seb" })
+                  .expect('{\n  "user": "seb"\n}', function () {
+                    var callArgs, eventCollectionName;
+                    callArgs = testRequest.getCall(0).args;
+                    eventCollectionName = callArgs[0];                
+
+                    eventCollectionName.should.equal("testEventCollectionName");
+                    done();
+                  });
     });
 
     it("should allow you to tag the event for a route from the configuration", function (done) {
-      true.should.be.false;
+      var testRequest = sinon.spy();
+      keenioMiddleware.keenClient.addEvent = testRequest;
+
+      request(app).get('/test')
+                  .send({ "user": "seb" })
+                  .expect('{\n  "user": "seb"\n}', function () {
+                    var callArgs, event;
+                    callArgs = testRequest.getCall(0).args;
+                    event = callArgs[1];                
+
+                    event.tag.should.equal("testTagName");
+                    done();
+                  });
     });
-    */
+
+    it("should send specific identity data to keen.io if the configuration mandated this", function (done) {
+      var testRequest = sinon.spy();
+      keenioMiddleware.keenClient.addEvent = testRequest;
+
+      request(app).get('/test')
+                  .send({ "user": "seb" })
+                  .expect('{\n  "user": "seb"\n}', function () {
+                    var callArgs, event;
+                    callArgs = testRequest.getCall(0).args;
+                    event = callArgs[1];                    
+
+                    event.identity.should.eql({ "test": "lies all lies - see later test" });
+                    done();
+                  });
+    });
 
   });
 
