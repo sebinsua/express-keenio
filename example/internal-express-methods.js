0e33a30827228adcad2909929d0868a203e77962
Seb Insua <me@sebinsua.com>
2014-01-26 17:30:08 +0000

    Response hooks exist but are not written.

--- example/internal-express-methods.js
+++ example/internal-express-methods.js
@@ -10,6 +10,8 @@ keenioMiddleware.configure({
 app.use(keenioMiddleware);
 
 app.get('/json-method', function (req, res) {
+  // @todo: Something to note is that we're letting objects become strings, to be come objects again
+  //        and this is shitty.
   // Behind the scenes uses res.send().
   res.json({
     special: 'hey',
@@ -18,6 +20,10 @@ app.get('/json-method', function (req, res) {
 });
 
 app.get('/jsonp-method', function (req, res) {
+  // @todo: If app.get('json callback something something') is set
+  // then you will need to parse something like this:
+  // typeof $callbackName === 'function' && $callbackName($json-string);
+  // @todo: Deal with in a similar way to how we are going to deal with .json().
   // Behind the scenes uses res.send().
   res.jsonp({
     special: 'hey',
@@ -26,17 +32,22 @@ app.get('/jsonp-method', function (req, res) {
 });
 
 app.get('/send-method', function (req, res) {
-  // @todo: Check whether I can pass in an object.
-  // @todo: Whether passing in a json strong causes capture? Eugh.
   res.send({
     special: 'hey',
     abc: 4
   });
 });
 
+app.get('/send-method-with-json-string', function (req, res) {
+  // Somehow Express decides this is a json object... Which is cool but strange imho.
+  res.send('{ "special": "hey", "abc": 4 }');
+});
+
 app.get('/send-method-forcing-json-in-strange-way', function (req, res) {
+  // Somehow Express decides this is a json object irrelevant of the Content-Type...
+  // Which means the original behaviour I started with is probably the right behaviour. SWEET. 
   res.set('Content-Type', 'application/json');
-  res.send("{ special: 'hey', abc: 4 }");
+  res.send('{ "special": "hey", "abc": 4 }');
 });
 
 app.get('/redirect-method', function (req, res) {
@@ -48,20 +59,20 @@ app.get('/redirect-method', function (req, res) {
 
 app.get('/redirect-method-with-2-args', function (req, res) {
   // Behind the scenes *DOES NOT* use res.send().
-  // But we would still like to capture from it, so
+  // @todo: But we would still like to capture from it, so
   // it's up for inclusion in the proxy response function. :)
   res.redirect(200, 'http://google.co.uk');
 });
 
 app.get('/redirect-method-with-path', function (req, res) {
   // Behind the scenes *DOES NOT* use res.send().
-  // But we would still like to capture from it, so
+  // @todo: But we would still like to capture from it, so
   // it's up for inclusion in the proxy response function. :)
   res.redirect(200, '../');
 });
 
 app.get('/render-method', function (req, res) {
-  // We *DEFINITELY* want this - and we definitely aren't currently gettin' it.
+  // @todo: We *DEFINITELY* want this - and we definitely aren't currently gettin' it.
   // idgaf ABOUT crashing something, I just want the data. idgaf.
   // What happens when the options is converted into a string and possible goes towards res.send?
   res.render('i-might-record-this-but-options-are-more-important', {
@@ -74,11 +85,13 @@ app.get('/render-method', function (req, res) {
 app.get('/send-file-method', function (req, res) {
   // I do not think I will need to worry about this, as it does not use res.send().
   // See also: [expressjs#res.sendFile](http://expressjs.com/api.html#res.sendFile)
+  // @todo: Create some way of getting out a simple reaction.
   res.sendFile('/uploads/download.txt');
 });
 
 app.get('/download-method', function (req, res) {
   // Likewise, see also: [expressjs#res.download](http://expressjs.com/api.html#res.download)
+  // @todo: Create a way of getting out a simple reaction.
   res.download('/uploads/download.txt', 'another-filename.txt');
 });
 
