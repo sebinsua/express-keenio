1fa1e5edb2ba04b57de6aadad483d8f80dda4261
Seb Insua <me@sebinsua.com>
2014-01-27 14:58:20 +0000

    ...

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
+// [express-keenio](http://github.com/sebinsua/express-keenio)
+//
 // See [lib/keenio-middleware](http://sebinsua.github.io/express-keenio/keenio-middleware.html).
 module.exports = require('./lib/keenio-middleware');
