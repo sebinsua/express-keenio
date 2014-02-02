d5632c2a04d88d4d90c5562f7df0b871803ac0d1
Seb Insua <me@sebinsua.com>
2014-02-02 23:43:02 +0000

    A little conversation with myself. Bear in mind aspects of eventual-schema are not fully working, but that what we have will suffices for express-keenio's purpose.

--- test/route-schemas.js
+++ test/route-schemas.js
@@ -7,3 +7,57 @@
 
 // I need a way of saving this information (collated instances *OR* eventual schema *OR* whitelist) to disk.
 // I need a way of loading this information (collated instances *OR* eventual schema *OR* whitelist) into a series of routes eventual schema objects.
+
+/*
+todo: Move to the route thing in keenio.
+EventualSchema.prototype._flattenProperties = function (obj, propertyDelimiter, arrayIdentifier) {
+    // Nested with . and []
+    delimiter = delimiter || '.';
+    arrayIdentifier = arrayIdentifier || '[]';
+};
+*/
+
+/*
+todo: move to the route thing in keenio.
+
+We don't create something like this (yet.):
+
+{
+    a.num: { _propertyCount: 3 },
+    a.arr: { _propertyCount: 3 },
+    b.arr[].name: { _propertyCount: 3 },
+    b.arr[].types: { _propertyCount: 3 },
+    b.value.type: { _propertyCount: 3 },
+    b.value.name: { _propertyCount: 3 },
+    c.arr: { _propertyCount: 3 }
+}
+
+// The will be injected in anyways.
+var hasMaximumProperties = function (eventualSchema) {
+  var MAX_PROPERTIES = 30;
+};
+
+var isAboveMinPropertyCount = function (eventualSchema) {
+  var MIN_PROPERTY_QUANTITY = 1;
+};
+
+var hasMaxInstances = function (routeSchema) {
+  var NUMBER_OF_INSTANCES_BEFORE_FREEZE = 500;    
+};
+
+var isBeyondMaxNumberOfDates = function (routeSchema) {
+  var NUMBER_OF_DAYS_BEFORE_FREEZE = 7;
+};
+
+describe('#_flattenProperties', function () {
+
+  it('should flatten properties given a nested object', function () {
+
+  });
+
+  it('should flatten properties including arrays with a special notation given a nested object', function () {
+
+  });
+
+});
+*/
